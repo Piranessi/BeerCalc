1f4d7d415471b6a19732ef676cdce7ed4ecc4c7b
Mateusz Gorzelak <matgorzelak@gmail.com>
2017-02-21 18:41:02 +0100

    Added homepage.

---
+++ BeerCalc/BeerCalc/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeerCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Tekst = DateTime.Now.Hour > 12 ? "Witamy :)" : "Masz zamiar pić przed 12? To znaczy, że powinieneś do nas dołączyć! :)";
+            return View();
+        }
+    }
+}
---
+++ BeerCalc/BeerCalc/Models/PersonDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeerCalc.Models
+{
+    public class PersonDB
+    {
+        public int Age { get; set; }
+        public float DrinkAge { get; set; } // tells what age was person when started drinking
+        public string Name { get; set; }
+        public int Expierience { get; set; }
+
+        public Dictionary<int, string> CountryDictionary { get; set; }
+    }
+}
